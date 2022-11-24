a5da42855e53d310870bd6ce2fdd84b1419cdea1
chertousovapolina <115408269+chertousovapolina@users.noreply.github.com>
2022-11-24 16:51:19 +0300

    Add files via upload

---
+++ ConsoleApplication12.cpp
@@ -0,0 +1,19 @@
+﻿
+//Чертоусова длина отрезка дз
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int c1, c2, x1, x2, y1, y2, c;
+    float d;
+    cin >> x1 >> y1 >> x2 >> y2;
+    c1 = x2 - x1;
+    c2 = y2 - y1;
+    c = c1 * c1 + c2 * c2;
+    d= sqrt(c);
+    cout << d;
+    
+}
+
